10dc1db9b0c0f9642527dae5c45999dab7902c16
nunutech40 <49187517+nunutech40@users.noreply.github.com>
2025-09-08 16:39:42 +0700

    add fonts assets to app and use it in product module

--- Product/Product/ProductView.swift
+++ Product/Product/ProductView.swift
@@ -19,6 +19,8 @@ public struct ProductView<Destination: View>: View {
             Image("real-madrid", bundle: Bundle(identifier: "com.dicoding.Common"))
                 .resizable()
                 .frame(width: 360, height: 120)
+            Text("Hi, i'm using Quicksand as Font")
+                .font(.custom("Quicksand-SemiBold", size: 20))
             NavigationLink(destination: action()) {
                 Text("Go to Detail")
             }
